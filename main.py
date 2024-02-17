acc46944118fbe29a6bfd8b7e8965c65b19cfe68
Juan D Hernandez G <juandavos941@gmail.com>
2024-02-16 21:59:47 -0500

    Using old model 3.5

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from langchain_openai import ChatOpenAI
 
 load_dotenv()
 
+OPENAI_MODEL = "gpt-3.5-turbo"
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 
 def main():
-    llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
+    llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name=OPENAI_MODEL)
     result = llm.invoke("Give five examples of animals that can fly.")
     print(result)
 
