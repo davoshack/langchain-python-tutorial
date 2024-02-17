5355b2d8c7eb77f2b1eae6a470e221faaab40fee
Juan D Hernandez G <juandavos941@gmail.com>
2024-02-17 12:10:57 -0500

    Download transcripts from YouTube Video

---
+++ transcripts.py
@@ -0,0 +1,29 @@
+import os
+
+import openai
+from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import OpenAIWhisperParser
+
+OPENAI_MODEL = "gpt-4"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+openai.api_key = OPENAI_API_KEY
+
+
+def main(url, file_path):
+    loader = GenericLoader(
+        YoutubeAudioLoader([url], os.path.dirname(file_path)), OpenAIWhisperParser()
+    )
+    docs = loader.load()
+    with open(file_path, "w", encoding="utf-8") as f:
+        for doc in docs:
+            f.write(doc.page_content)
+
+    return docs
+
+
+if __name__ == "__main__":
+    youtube_url = "https://youtu.be/CYTwGx43SzY"
+    file_path = "3_things_never_do_in_the_morning.txt"
+
+    main(youtube_url, file_path)
